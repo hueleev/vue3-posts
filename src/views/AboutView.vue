924bffcfb52b88c6dbfeb48bf9351ce630feed2e
Hyungwon Lee <hueleev@gmail.com>
2022-09-05 15:09:40 +0900

    2_vueRouter

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+	<h2>aboutView</h2>
+	<p>{{ $route.path }}</p>
+	<button class="btn btn-primary" @click="$router.push('/')">
+		Home으로 이동
+	</button>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+console.log('router.path:', router.path);
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+	<h2>home view</h2>
+	<p>{{ $route }}</p>
+	<p>{{ $router }}</p>
+	<button class="btn btn-primary" @click="goAboutPage">About으로 이동</button>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goAboutPage = () => {
+	router.push('/about');
+};
+</script>
+
+<style lang="scss" scoped></style>
