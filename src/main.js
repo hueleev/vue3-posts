eaec05f0020c740f66ea60b5be33ad6b7e7c15da
Hyungwon Lee <hueleev@gmail.com>
2022-09-14 15:16:18 +0900

    13_plugin

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/router';
+import funcPlugins from './plugins/func';
+import objPlugins from './plugins/obj';
+import person from './plugins/person';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.use(router);
+app.use(funcPlugins);
+app.use(objPlugins, { name: '짐코딩' });
+app.use(person, { name: '홍길동' });
+app.mount('#app');
+
 import 'bootstrap/dist/js/bootstrap.js';
 
 console.log('BASE_URL: ', import.meta.env.VITE_APP_API_URL);
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,19 +12,33 @@
 	<AppGrid :items="items" v-slot="{ item }" col-calss="col-3">
 		<AppCard>{{ item }}</AppCard>
 	</AppGrid>
+	<hr class="my-4" />
+	<h2>{{ person.name }}</h2>
+	<button class="btn btn-primary" @click="person.say">click person</button>
 </template>
+
+<script>
+export default {
+	created() {
+		// console.log(this.$person.name);
+		// this.$person.say();
+	},
+};
+</script>
 
 <script setup>
 import { useRouter } from 'vue-router';
 import AppCard from '@/components/AppCard.vue';
 import AppGrid from '@/components/AppGrid.vue';
-import { ref } from 'vue';
+import { inject, ref } from 'vue';
 const router = useRouter();
 const goAboutPage = () => {
 	router.push('/about');
 };
 
 const items = ref(['사과', '딸기', '포도', '바나나']);
+const person = inject('person');
+console.log('person.name', person.name);
 </script>
 
 <style lang="scss" scoped></style>
