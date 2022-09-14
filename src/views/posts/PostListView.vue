634e866a262ed4177435fe1d1ffeb290e640087b
Hyungwon Lee <hueleev@gmail.com>
2022-09-14 14:05:33 +0900

    12_teleport_modal

--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -14,6 +14,7 @@
 					:content="item.content"
 					:created-at="item.createdAt"
 					@click="goPage(item.id)"
+					@modal="openModal(item)"
 				></PostItem>
 			</template>
 		</AppGrid>
@@ -25,6 +26,15 @@
 			:page-count="pageCount"
 			@page="page => (params._page = page)"
 		/>
+		<Teleport to="#modal">
+			<PostModal
+				v-model="show"
+				:title="modalTitle"
+				:content="modalContent"
+				:created-at="modalCreatedAt"
+			>
+			</PostModal>
+		</Teleport>
 		<template v-if="posts && posts.length > 0">
 			<hr class="my-5" />
 			<AppCard>
@@ -40,6 +50,7 @@ import PostDetailView from '@/views/posts/PostDetailView.vue';
 import AppPagination from '@/components/AppPagination.vue';
 import AppCard from '@/components/AppCard.vue';
 import AppGrid from '@/components/AppGrid.vue';
+import PostModal from '@/components/posts/PostModal.vue';
 import PostFilter from '@/components/posts/PostFilter.vue';
 
 import { getPosts } from '@/api/posts';
@@ -88,6 +99,18 @@ const goPage = id => {
 		// },
 		// hash: 'world',
 	});
+};
+
+// modal
+const show = ref(false);
+const modalTitle = ref('');
+const modalContent = ref('');
+const modalCreatedAt = ref('');
+const openModal = ({ title, content, createdAt }) => {
+	show.value = true;
+	modalTitle.value = title;
+	modalContent.value = content;
+	modalCreatedAt.value = createdAt;
 };
 </script>
 
